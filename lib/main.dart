71e9dd6a18e008287db061144c2045da69ca08e9
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2019-12-06 16:54:23 -0300

    Configuração de tema customizado para plataforma

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,96 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+void main() => runApp(MyApp());
 
-  Firestore.instance.collection("mensagens").snapshots().listen((snapshot) {
-    snapshot.documents.forEach((d) => {
-      debugPrint(d.data.toString())
-    });
-  });
+final ThemeData kiOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light
+);
 
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400]
+);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "App ChatOnline",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS ? kiOSTheme : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title:  Text("Chat App"),
+          centerTitle: true,
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextCompose(),
+            )
+          ],                    
+        ),
+      ),
+    );
+  }
+}
+
+
+class TextCompose extends StatefulWidget {
+  @override
+  _TextComposeState createState() => _TextComposeState();
+}
+
+class _TextComposeState extends State<TextCompose> {
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS ? BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey[200]))
+        ) : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: (){
+
+                },
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration.collapsed(hintText: "Enviar uma mensagem")
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
