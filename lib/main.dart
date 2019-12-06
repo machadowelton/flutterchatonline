943f7b4ff802d12a9ebc029a9afdd6d3870eb6ea
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2019-12-06 13:45:17 -0300

    Exemplo de como escrever no firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
-  Firestore.instance.collection("teste").document("test").setData({"teste":"teste"});
+  Firestore.instance
+      .collection("mensagens")
+      .document()
+      .collection("arqmidia")
+      .document()
+      .setData({"from": "Welton", "texto": "Olá"});
 
   runApp(MaterialApp(
     home: MyApp(),
   ));
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
